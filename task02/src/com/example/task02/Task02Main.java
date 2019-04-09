4b3972f979139d3bb50e1238aa7d7833099111cb
wisedog <psnetwork1138@gmail.com>
2019-04-09 17:41:47 +0500

    Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,5 +1,7 @@
 package com.example.task02;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.io.IOException;
 
 public class Task02Main {
@@ -9,5 +11,43 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int firstByte = 1;
+        int secondByte;
+
+        while (firstByte != -1) {
+            if (firstByte != 13) {
+                firstByte = System.in.read();
+            }
+
+            if (firstByte != 13 && firstByte != -1) {
+                System.out.write(firstByte);
+                firstByte = 0;
+                continue;
+            }
+
+            if(firstByte == 13){
+                if((secondByte = System.in.read()) == 10){
+                    System.out.write(secondByte);
+                    firstByte = 0;
+                    continue;
+                }
+                if(secondByte == 13){
+                    System.out.write(firstByte);
+                    firstByte = secondByte;
+                    secondByte = 0;
+                    continue;
+                }
+                else if(secondByte != -1){
+                    System.out.write(firstByte);
+                    System.out.write(secondByte);
+                    firstByte = 0;
+                    continue;
+                }
+                else{
+                    System.out.write(firstByte);
+                    break;
+                }
+            }
+        }
     }
 }
