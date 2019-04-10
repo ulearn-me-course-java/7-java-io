bd45476d27c2d7c9bf20371d34efc228d389f1f1
Henkka <henkkawar@gmail.com>
2019-04-10 20:46:46 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,11 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+    public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
         double x = 0;
@@ -16,10 +13,11 @@ public class Task04Main {
             String next = scanner.next();
             try {
                 x += Double.parseDouble(next);
-            } catch (Exception e) {
-                //ignore
+            } catch (NumberFormatException e) {
+
             }
         }
-        System.out.println(String.format("%.6f", x));
+        System.out.println(String.format(Locale.US, "%.6f", x));
+
     }
 }
