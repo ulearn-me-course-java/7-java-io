7d1dc712c15378a12092a75413d1727b2103f3e6
Henkka <henkkawar@gmail.com>
2019-04-10 18:21:40 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -10,11 +10,13 @@ import static java.lang.System.out;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        try(InputStreamReader inputStreamReader = new InputStreamReader(System.in)) {
-            Scanner s = new Scanner(inputStreamReader).useDelimiter("\\z");
-            String result = s.hasNext() ? s.next() : "";
-            out.write(result.replaceAll("\r\n", "\n").getBytes());
+        try (Scanner scanner = new Scanner(new InputStreamReader(System.in)).useDelimiter("\\z")) {
+            if (scanner.hasNext())
+                out.write(scanner.next()
+                        .replaceAll("\r\n", "\n")
+                        .getBytes());
         }
+
         System.out.flush();
     }
 }
