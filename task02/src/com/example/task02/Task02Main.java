104ac9eab15729933b44f790cb00c1afe8b11654
Max <uue33@mail.ru>
2019-04-14 17:31:01 +0500

    Иконникова Анастасия. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,21 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        int currentByte = inputStream.read();
+        int nextByte;
+        while (currentByte > -1) {
+            nextByte = inputStream.read();
+            if (!(currentByte == 13 && nextByte == 10))
+                outputStream.write(currentByte);
+            currentByte = nextByte;
+        }
+        System.out.flush();
     }
 }
