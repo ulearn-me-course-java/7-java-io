7070abd9584fbb46eaaf1636584f3981a9ba88c6
AllaOsipyan <allosipyan@mail.ru>
2019-04-14 19:35:24 +0500

    Осипян Пайцар.Задание04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,15 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
+        double sum = 0.0;
+        while (scanner.hasNext()) {
+            try {
+                sum += Double.parseDouble(scanner.next());
+            } catch (Exception e) {
+            }}
+
+        System.out.println(String.format(Locale.ENGLISH,"%.6f", sum));
     }
 }
