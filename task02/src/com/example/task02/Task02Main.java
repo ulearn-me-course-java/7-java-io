9db213c892d6170963bf376c55d68cd445534265
Pavel <pavel.zaborin@inbox.ru>
2019-04-15 14:54:04 +0500

    Заборин Павел. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,6 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +9,19 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        InputStreamReader reader = new InputStreamReader(System.in);
+        int currentSymbol;
+        int prevSymbol = -1;
+
+        while ((currentSymbol = reader.read()) != -1) {
+            if (!(prevSymbol == 13 && currentSymbol == 10) && prevSymbol != -1) {
+                System.out.print((char) prevSymbol);
+            }
+            prevSymbol = currentSymbol;
+        }
+
+        if (prevSymbol != -1){
+            System.out.print((char) prevSymbol);
+        }
     }
 }
