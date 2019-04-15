2cb48ac664fb94acebf839df6011c684e6365396
AllaOsipyan <allosipyan@mail.ru>
2019-04-15 19:45:30 +0500

    Осипян Пайцар.Задание02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,6 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +9,21 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        Reader reader = new InputStreamReader(System.in);
+        int first = reader.read();
+        int second;
+        while (first != -1) {
+            try {
+                second = reader.read();
+                if (!(first == 13 && second == 10))
+                    System.out.write(first);
+                first = second;
+
+            } catch (Exception e) {
+                System.out.println("Введены неправильные значения.");
+            }
+        }
+        System.out.flush();
     }
+
 }
