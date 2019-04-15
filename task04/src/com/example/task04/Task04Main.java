e3cfc12f65b1f4bfb1ff1a748214c2f67c8acebb
Pavel <pavel.zaborin@inbox.ru>
2019-04-15 15:56:11 +0500

    Заборин Павел. Задание 04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,16 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+
+        while (scanner.hasNext()){
+            String buff = scanner.next();
+            try {
+                sum += Double.parseDouble(buff);
+            }
+            catch (Exception e){}
+        }
+        System.out.printf(Locale.ENGLISH, "%.6f", sum);
     }
 }
