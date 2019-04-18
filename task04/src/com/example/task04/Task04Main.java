d5779f2bc66230648fa376a291e93e57c2f7c2ca
Vitaliy Smichkovskiy <32594950+ProgressiveArt@users.noreply.github.com>
2019-04-18 22:02:39 +0300

    Задание 04 Смычковский В.
    # Задание 04 Поиск чисел
    
    Напишите программу, читающую текст из System.in и выводящую в System.out сумму всех встреченных в тексте вещественных чисел с точностью до шестого знака после запятой. Числом считается последовательность символов, отделенная от окружающего текста пробелами или переводами строк и успешно разбираемая методом Double.parseDouble.
    
    #### Sample Input 1:
    
    1 2 3
    
    #### Sample Output 1:
    
    6.000000
    
    #### Sample Input 2:
    
    a1 b2 c3
    
    #### Sample Output 2:
    
    0.000000
    
    #### Sample Input 3:
    
    -1e3
    18 .111 11bbb
    
    #### Sample Output 3:
    
    -981.889000

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,33 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+// чтобы протестировать свое решение, вам нужно:
+// - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
+// - запустить программу
+// - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        Scanner scan = new Scanner(System.in);
+
+        double sum = 0;
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+
+            String[] splitStr = line.split(" ");
+
+            for (String word : splitStr) {
+                try {
+                    sum += Double.parseDouble(word);
+                } catch (Exception ex) {
+                    continue;
+                }
+            }
+        }
+
+        System.out.println(new DecimalFormat("#0.000000").format(sum).replace(',', '.'));
     }
 }
