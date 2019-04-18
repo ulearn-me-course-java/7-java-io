05a321fa59786425a37d66997bf6e6c0ee5ba1ba
uytera <uytera12@yandex.ru>
2019-04-18 10:13:18 +0500

    Даниил Бакулин Задание02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,28 @@
 package com.example.task02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int symbol;
+        int previousSymbol = -1;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while ((symbol = reader.read()) > -1) {
+        if (previousSymbol == -1) {
+            previousSymbol = symbol;
+            continue;
+        }
+
+        if (!(previousSymbol == 13 && symbol == 10)) {
+            System.out.print((char) previousSymbol);
+            System.out.flush();
+        }
+
+        previousSymbol = symbol;
+    }
+        if (previousSymbol != -1)System.out.print((char) previousSymbol);
     }
 }
