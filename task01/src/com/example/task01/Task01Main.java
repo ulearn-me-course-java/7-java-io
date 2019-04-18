2cff7be6a78b10e03d891d1b5e8e80aff477fc18
Vitaliy Smichkovskiy <32594950+ProgressiveArt@users.noreply.github.com>
2019-04-18 07:26:20 +0300

    Задание 01 Смычковский В.
    # Задание 01 Контрольная сумма
    
    Напишите метод, читающий входной поток и вычисляющий контрольную сумму прочитанных данных.
    
    Контрольная сумма данных вычисляется по следующему алгоритму:
    
    1. Контрольная сумма представляет собой число типа int. Контрольная сумма пустого набора данных равна нулю.
    2. Контрольная сумма непустого набора данных вычисляется по следующей рекуррентной формуле: C\[n+1\]=rotateLeft(C\[n\]) xor b\[n+1\] , где C\[n\] — контрольная сумма первых n байт данных, rotateLeft — циклический сдвиг бит числа на один бит влево (чтобы не изобретать велосипед, используйте Integer.rotateLeft), b\[n\] — n-ный байт данных.
    Поскольку метод не открывал данный InputStream, то и закрывать его он не должен. Выброшенное из методов InputStream исключение должно выбрасываться из метода.
    
    #### Пример
    На вход подан InputStream, последовательно возвращающий три байта: 0x33 0x45 0x01. В качестве контрольной суммы должно быть возвращено число 71.

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,5 +1,6 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,14 +9,22 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
+        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{})));
+        System.out.println(checkSumOfStream(null));
 
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) throw new IllegalArgumentException();
+
+        int checkSum = 0;
+        int currByte = 0;
+
+        while (currByte > -1) {
+            checkSum = Integer.rotateLeft(checkSum, 1) ^ currByte;
+            currByte = inputStream.read();
+        }
+        return checkSum;
     }
 }
