c5b302d68148af1a804caec145de9dd3295b9f7c
Алексей Ступников <stuq11@gmail.com>
2019-04-18 19:08:33 +0500

    Задача 02 решена

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int prev, cur = System.in.read();
+        while (cur > -1) {
+            prev = cur;
+            cur = System.in.read();
+            if (!((prev == 0x0D) && (cur == 0x0A)))
+                System.out.write(prev);
+        }
+        System.out.flush();
     }
 }
