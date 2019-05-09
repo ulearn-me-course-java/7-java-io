a660c86890a2b16d83f025693316cf28bda13e62
Mystache <Mystache@yandex.ru>
2019-05-09 22:09:02 +0500

    Solution #4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
         Scanner scanner = new Scanner(System.in);
         double x = 0;
         while (scanner.hasNext()) {
@@ -17,9 +12,10 @@ public class Task04Main {
             try {
                 x += Double.parseDouble(next);
             } catch (Exception e) {
-                //ignore
+                // ignore
             }
         }
-        System.out.println(String.format("%.6f", x));
+
+        System.out.println(String.format("%.6f", x).replace(",", "."));
     }
 }
