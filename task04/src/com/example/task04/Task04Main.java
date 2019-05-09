1aed3aed4e3ed806552a35250e6190cf8619e01e
Mystache <Mystache@yandex.ru>
2019-05-09 22:28:35 +0500

    Решение задания #4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,14 +2,10 @@ package com.example.task04;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
         Scanner scanner = new Scanner(System.in);
         double x = 0;
         while (scanner.hasNext()) {
@@ -17,9 +13,10 @@ public class Task04Main {
             try {
                 x += Double.parseDouble(next);
             } catch (Exception e) {
-                //ignore
+                // ignore
             }
         }
-        System.out.println(String.format("%.6f", x));
+
+        System.out.println(String.format(Locale.ENGLISH, "%.6f", x));
     }
 }
