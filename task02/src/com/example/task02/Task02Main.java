e433b636f3c29dd5704455cd30fc4834316fc8e2
Alexey <alexey.pashnin@mail.ru>
2019-07-04 04:34:42 +0400

    Пашнин Алексей. Задание 02.

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,26 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        if (System.in.available() != 0) {
+            int[] output = new int[System.in.available()];
+            output[0] = System.in.read();
+            int current = -1;
+            int resultedLength = 0;
+            for (int c = 1; (current = System.in.read()) > -1; c++) {
+                if (output[c - 1] == 13 && current == 10) {
+                    output[--c] = current;
+                } else {
+                    output[c] = current;
+                }
+                resultedLength = c;
+            }
+
+            output = Arrays.copyOf(output, ++resultedLength);
+            for (int c = 0; c < output.length; c++) {
+                System.out.write(output[c]);
+            }
+            System.out.flush();
+        }
     }
 }
