2b9c0393acd0f0f72dc34754cc95a53bfe75abe8
Alexey <alexey.pashnin@mail.ru>
2019-07-04 18:57:05 +0400

    Пашнин Алексей. Задание 04.

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Stack;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,24 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        if (System.in == null) throw new IllegalArgumentException();
+
+        double result = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int c = System.in.available(); c > -1; c--) {
+            char current = (char) System.in.read();
+            if (current == ' ' || current == '\n' || c == 0) {
+                try {
+                    result += Double.parseDouble(stringBuilder.toString());
+                } catch (NumberFormatException e) {
+
+                }
+                stringBuilder.setLength(0);
+            } else {
+                stringBuilder.append(current);
+            }
+        }
+        System.out.format(Locale.US, "%.6f", result);
     }
 }
