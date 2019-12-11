f3f43d20e6b7e8a1e893babc2e2c616007a3e14d
devil <devilaraxgada@gmail.com>
2019-12-11 15:16:24 +0500

    Решение задачи 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,13 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+        int rByte, sum = 0;
+        while ((rByte = inputStream.read()) != -1) {
+            sum = Integer.rotateLeft(sum, 1) ^ rByte;
+        }
+        return sum;
     }
 }
