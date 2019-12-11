fb57f08f550e562df6ed4843c95f15771553258d
devil <devilaraxgada@gmail.com>
2019-12-11 15:20:14 +0500

    Решение задачи 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,19 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+    }
 
-        System.out.println("0.0");
+    public static void printSum() {
+        Scanner s = new Scanner(System.in);
+        double sum = 0;
+        while (s.hasNext()) {
+            String buff = s.next();
+            try {
+                sum += Double.parseDouble(buff);
+            } catch (Exception e) {
+
+            }
+        }
+        System.out.printf(Locale.ENGLISH, "%.6f", sum);
     }
 }
