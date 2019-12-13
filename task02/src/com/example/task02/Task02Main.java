7ad9e11c16fa2af64c0688455138341a3045da56
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-12-13 16:55:41 +0500

    [Матвеева] Решение задаяи 7.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,25 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        toUnixEOL();
+    }
+
+    public static void toUnixEOL() throws IOException {
+        int[] readBytes = new int[2];
+        readBytes[0] = System.in.read();
+        readBytes[1] = System.in.read();
+
+        while (readBytes[0] != -1) {
+            if (readBytes[0] == 13 && readBytes[1] == 10) {
+                System.out.write(10);
+                readBytes[0] = System.in.read();
+                readBytes[1] = System.in.read();
+            } else {
+                System.out.write(readBytes[0]);
+                readBytes[0] = readBytes[1];
+                readBytes[1] = System.in.read();
+            }
+        }
+        System.out.flush();
     }
 }
