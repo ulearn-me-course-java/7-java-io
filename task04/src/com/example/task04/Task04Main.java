f0bf29ca901c98ad05862ceaa46ff9b1aad5cada
rokoman13 <rokoman13@users.noreply.github.com>
2019-12-16 14:28:47 +0500

    Popov Evgeny task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,16 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in);
+        double x = 0;
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            try {
+                x += Double.parseDouble(next);
+            } catch (Exception e) {
+                //ignore
+            }
+        }
+        System.out.println(String.format(Locale.ENGLISH, "%.6f", x));
     }
 }
