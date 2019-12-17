598640913e3db91e7fdadcb46b27e04f43b5f31f
genwarhunter <kostyarik98@mail.ru>
2019-12-17 20:17:22 +0500

    commit

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,23 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        strFromWinToUnix();
+    }
+
+    public static void strFromWinToUnix() throws IOException {
+        int current = System.in.read();
+        while (current != -1) {
+            int next = System.in.read();
+            if (next == -1) {
+                System.out.write(current);
+            } else if (current == 13 && next == 10) {
+                System.out.write(next);
+                next = System.in.read();
+            } else {
+                System.out.write(current);
+            }
+            current = next;
+        }
+        System.out.flush();
     }
 }
