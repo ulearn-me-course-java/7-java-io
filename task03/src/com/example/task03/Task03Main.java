f42a435de4f94453b870c30b732269829c999ab1
Валерия Самсонова <samsonova147@hotmail.com>
2019-12-18 10:58:32 +0500

    [Самсонова] Решение задачи 7.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,8 @@ package com.example.task03;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -16,6 +18,15 @@ public class Task03Main {
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         // your implementation here
-        return "";
+        if (inputStream == null || charset == null) {
+            throw new IllegalArgumentException();
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset));
+        String str = "";
+        int current = 0;
+        while ((current = reader.read()) != -1) {
+            str += (char) current;
+        }
+        return str;
     }
 }
