0140b997bb47444b91c046142044ad29d73c2fef
Валерия Самсонова <samsonova147@hotmail.com>
2019-12-18 11:08:57 +0500

    [Самсонова] Решение задачи 7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,19 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        findDigit();
+    }
 
-        System.out.println("0.0");
+    public static void findDigit() {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        while (scanner.hasNext()) {
+            String buf = scanner.next();
+            try {
+                sum += Double.parseDouble(buf);
+            } catch (Exception e) {
+            }
+        }
+        System.out.printf(Locale.ENGLISH, "%.6f", sum);
     }
 }
