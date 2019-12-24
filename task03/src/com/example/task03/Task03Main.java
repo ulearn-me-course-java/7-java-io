0426f2f5aefd1b7fbfd917e2c5c00401526544b8
MintAnnetta <mintannetta@gmail.com>
2019-12-24 17:00:58 +0500

    Задача 7.3 решена! :D

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,8 @@ package com.example.task03;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.io.InputStreamReader;
+import java.io.DataInputStream;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -16,6 +18,12 @@ public class Task03Main {
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         // your implementation here
-        return "";
+        if (inputStream == null || charset == null) {
+            throw new IllegalArgumentException();
+        }
+
+        byte[] buf = new byte[inputStream.available()];
+        int resive = inputStream.read(buf);
+        return new String(buf, charset);
     }
 }
