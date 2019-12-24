9a564693ba9f356a07805ad7c9abb1d2662dbec3
rokoman13 <rokoman13@users.noreply.github.com>
2019-12-24 17:24:23 +0500

    Popov Evgeny task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,16 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int prevByte = System.in.read();
+        int nextByte;
+
+        while (prevByte >= 0) {
+            nextByte = System.in.read();
+            if (!(prevByte == 13 && nextByte == 10)) {
+                System.out.write(prevByte);
+            }
+            prevByte = nextByte;
+        }
+        System.out.flush();
     }
 }
