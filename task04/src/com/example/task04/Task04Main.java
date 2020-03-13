7a55e5a5e317bc5b681ac7f952a8d51f11135e47
stasenash <staslisenash@gmail.com>
2020-03-13 11:48:17 +0500

    Бахарева Антонина З.04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,21 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in);
+        double result = 0;
+        while ((scanner.hasNext())) {
+            try {
+                result += Double.parseDouble(scanner.next());
+            }
+            catch (Exception e) {
+            }
+        }
+        System.out.format(Locale.US, "%.6f", result);
     }
 }
