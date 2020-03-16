58df656cc7fe4c64fb14f43df89afb8aa86a6da4
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-16 17:13:43 +0500

    task 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,26 @@
 package com.example.task04;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        while (scanner.hasNext()) {
+            sum += tryParseDouble(scanner.next());
+        }
 
-        System.out.println("0.0");
+        System.out.append(String.format("%.6f", sum).replace(",", "."));
+        System.out.flush();
+    }
+
+    static double tryParseDouble(String s) {
+        double result;
+        try {
+            result = Double.parseDouble(s);
+        } catch (Exception ex) {
+            return 0.0;
+        }
+        return result;
     }
 }
