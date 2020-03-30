14c1c3e702d6bd67fab80953da072eb38f5df38c
yeroxo <yeroxo@gmail.com>
2020-03-30 15:49:26 +0500

    Колыванов Артем При 201 task 7.1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -5,17 +5,17 @@ import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        int res = 0;
+        int data;
+        if (inputStream == null) throw new IllegalArgumentException("Stream empty");
+
+        while((data = inputStream.read()) >= 0){
+            res = Integer.rotateLeft(res, 1) ^ data;
+        }
+
+        return  res;
     }
 }
