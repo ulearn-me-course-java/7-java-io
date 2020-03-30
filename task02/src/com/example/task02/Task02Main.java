c225f8eca48c6fffd198ad55f8c21bcb3d2511ef
yeroxo <yeroxo@gmail.com>
2020-03-30 17:31:20 +0500

    Колыванов Артем При 201 task 7.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,11 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        byte[] bytes = new byte[System.in.available()];
+        System.in.read(bytes);
+        System.out.append(new String(bytes).replaceAll("\r\n","\n"));
     }
 }
