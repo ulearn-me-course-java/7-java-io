1cad58092e8cbec2e7642d0e786da266369e36a4
LordOfTheDeadlines <vedroid28@mail.ru>
2020-03-31 11:09:52 +0500

    Сидорчук Анастасия. 7.02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,15 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        fromWindowsToUnix();
+    }
+
+    public static void fromWindowsToUnix() throws IOException {
+       byte[] input=new byte[System.in.available()];
+       System.in.read(input);
+       System.out.print(new String(input).replace("\r\n","\n"));
     }
 }
