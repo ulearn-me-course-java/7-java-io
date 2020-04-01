581b674a16ddebb25483b7d7f4946e29a205821e
DarenOwl <DarenOwl@users.noreply.github.com>
2020-04-01 13:30:09 +0500

    Андреева Юлия. Задание 04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,21 @@
 package com.example.task04;
 
-import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.UK);
+        double sum = 0;
 
-        System.out.println("0.0");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble())
+                sum += scanner.nextDouble();
+            else
+                scanner.next();
+        }
+
+        System.out.println(String.format(Locale.UK, "%.6f", sum));
     }
 }
