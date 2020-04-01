cae34205027a26b14d1e81941b423de0bb4c5867
Ekaterina <malyavkina-00@mail.ru>
2020-04-01 09:25:40 +0500

    Малявкина Екатерина Задание1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,7 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +16,15 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        int sum = 0;
+        if(inputStream == null) throw new IllegalArgumentException("Input Stream is not exist");
+
+        int dataByte = inputStream.read();
+        while(dataByte >= 0) {
+            sum = Integer.rotateLeft(sum, 1) ^ dataByte;
+            dataByte = inputStream.read();
+        }
+        return sum;
+
     }
 }
