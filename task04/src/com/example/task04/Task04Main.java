68185dab7fa40d570d8d3cdd8a2e214088ad823f
Наби Гадимов <gadimovnabi@gmail.com>
2020-04-02 01:47:01 +0500

    Гадимов Наби. Задание 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,27 @@
 package com.example.task04;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        Scanner scanner = new Scanner(System.in); //входной поток данных
+        double sum = 0;
+        while (scanner.hasNext()) { //пока в сканнере есть слова
+            sum += tryParseDouble(scanner.next()); //в сумму добавляется число(после строки)
+        }
+        System.out.append(String.format("%.6f", sum).replace(",", ".")); //обновляет значение объекта, добавление
+    }
 
-        System.out.println("0.0");
+    private static double tryParseDouble(String str) {
+        double sum;
+        try {
+            sum = Double.parseDouble(str); //преобразует строку в число типа Double
+        }
+        catch (Exception ex) {
+            return 0.0;
+        }
+        return sum;
     }
 }
