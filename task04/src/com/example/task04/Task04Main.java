742ad184ba8eb185b9c86c3078f88017556a8099
Ekaterina <malyavkina-00@mail.ru>
2020-04-01 13:04:50 +0500

    Малявкина Екатерина Задание4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,7 @@
 package com.example.task04;
 
-import java.io.IOException;
+import java.io.IOException;import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +9,22 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        while (scanner.hasNext()) {
+            sum += tryParseDouble(scanner.next());
+        }
+        System.out.append(String.format("%.6f", sum).replace(",", "."));
+        System.out.flush();
+    }
 
-        System.out.println("0.0");
+    private static double tryParseDouble(String str) {
+        double sum;
+        try {
+            sum = Double.parseDouble(str);
+        } catch (Exception ex) {
+            return 0.0;
+        }
+        return sum;
     }
 }
