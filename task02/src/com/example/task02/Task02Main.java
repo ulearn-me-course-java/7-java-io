363a5fa94d581900b4ed46d8e304041bb7bb91d5
Id.Io.T <kdevyatkov-ilya@mail.ru>
2020-04-02 00:07:02 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        byte[] bytes = new byte[System.in.available()];
+        System.in.read(bytes);
+        System.out.append(new String(bytes).replace("\r\n", "\n"));
     }
 }
