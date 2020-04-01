09796e70a603a0335eceae2c218d4145985ba2c5
Наби Гадимов <gadimovnabi@gmail.com>
2020-04-01 19:45:41 +0500

    Гадимов Наби. Задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.*; //импорт всех классов для работы с вводом-выводом
+
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,9 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        byte[] bytes = new byte[System.in.available()]; //создание массива байтов длинны потока(in - поток, avalible - количество чисел, которое можно считать)
+        System.in.read(bytes);
+
+        System.out.append(new String(bytes).replaceAll("\r\n","\n"));
     }
 }
