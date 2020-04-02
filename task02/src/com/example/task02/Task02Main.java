751dbb0ca488f6b3cd15a4549f254f1668523c6f
Ekaterina <slalam.ww@gmail.com>
2020-04-02 19:49:50 +0500

    Абрамкова, 7.02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,25 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+    public static void main (String[] strings) throws IOException {
+        windowsToUnix(System.in, System.out);
     }
+
+    public static void windowsToUnix(InputStream in, OutputStream out) throws IOException {
+
+        int oneByte = in.read(), twoByte;
+        while(oneByte != -1) {
+            twoByte = in.read();
+            if (!(twoByte == 10 && oneByte == 13)) out.write(oneByte);
+
+            oneByte = twoByte;
+        }
+
+        out.flush();
+    }
+
 }
