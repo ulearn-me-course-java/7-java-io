a2acd8791abd9e4dc0f90045d88c693a00eb9d22
Sergey <just1ce013@mail.ru>
2020-04-02 18:02:30 +0500

    Обласов Сергей Здание 7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,7 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +9,24 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        if (System.in == null) throw new IllegalArgumentException();
 
-        System.out.println("0.0");
+        double result = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int c = System.in.available(); c > -1; c--) {
+            char current = (char) System.in.read();
+            if (current == ' ' || current == '\n' || c == 0) {
+                try {
+                    result += Double.parseDouble(stringBuilder.toString());
+                } catch (NumberFormatException e) {
+
+                }
+                stringBuilder.setLength(0);
+            } else {
+                stringBuilder.append(current);
+            }
+        }
+        System.out.format(Locale.US, "%.6f", result);
     }
 }
