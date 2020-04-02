a16f39ef8786d43c5857279a9077e8a884894327
xhnrdt00n <xoine.un@gmail.com>
2020-04-02 20:35:34 +0500

    Жмакина Алина. Задание 7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,20 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        double result = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            try {
+                result += Double.parseDouble(scanner.next());
+            } catch (Exception e) {
 
-        System.out.println("0.0");
+            }
+        }
+        System.out.printf(Locale.UK, "%.6f", result);
     }
 }
