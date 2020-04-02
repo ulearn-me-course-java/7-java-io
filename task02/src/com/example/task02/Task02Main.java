cfa4ef3bc22892fc531e500a59cb38ac0f0bd958
LordOfTheDeadlines <vedroid28@mail.ru>
2020-04-02 19:03:14 +0500

    Сидорчук Анастасия. 7.02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,12 +4,21 @@ import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        fromWindowsToUnix();
+        fromWindowsToUnix(System.in,System.out);
     }
 
-    public static void fromWindowsToUnix() throws IOException {
-       byte[] input=new byte[System.in.available()];
-       System.in.read(input);
-       System.out.print(new String(input).replace("\r\n","\n"));
+    public static void fromWindowsToUnix(InputStream in,OutputStream out) throws IOException {
+        int firstByte = in.read();
+        int secondByte;
+
+        while(firstByte != -1){
+            secondByte = in.read();
+            if (!(secondByte == 10 && firstByte == 13)){
+                out.write(firstByte);
+            }
+            firstByte = secondByte;
+        }
+        out.flush();
     }
+
 }
