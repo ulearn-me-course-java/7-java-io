22cc15a4e2ac7fac84802821f7f7686986d6029f
Jin <jin1310@icloud.com>
2020-04-02 18:32:44 +0500

    task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,9 @@
 package com.example.task03;
 
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -15,7 +17,16 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if(inputStream == null) throw new IllegalArgumentException(); //
+
+        StringBuilder strBuilder = new StringBuilder();
+        String line = null;
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,charset));//это класс обертка и туда передается инпутстримридер и еще обертка на инпетстриом
+        while((line = bufferedReader.readLine()) != null){//пока есть строка
+            strBuilder.append(line);
+        }
+
+        return strBuilder.toString();
     }
 }
