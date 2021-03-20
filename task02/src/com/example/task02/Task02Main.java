8dc232c284cdd907d719ed8114240029a64ec299
LPNHR <lpnhr.style@gmail.com>
2021-03-20 21:51:14 +0500

    Григоричев 7.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,42 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Task02Main {
+
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        ArrayList<Integer> lines = getLines();
+        deleteAllReturnsCarriages(lines);
+        outLines(lines);
     }
+
+    private static void outLines(ArrayList<Integer> lines) {
+        for (Integer integer : lines) {
+            System.out.write(integer);
+        }
+        System.out.flush();
+    }
+
+    private static void deleteAllReturnsCarriages(ArrayList<Integer> lines) {
+        int previousValue = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            int currentValue = lines.get(i);
+            if (previousValue == 13 && currentValue == 10) {
+                lines.remove(i - 1);
+                i--;
+            }
+            previousValue = currentValue;
+        }
+    }
+
+    private static ArrayList<Integer> getLines() throws IOException {
+        int input;
+        ArrayList<Integer> lines = new ArrayList<>();
+        while ((input = System.in.read()) != -1) {
+            lines.add(input);
+        }
+        return lines;
+    }
+
 }
