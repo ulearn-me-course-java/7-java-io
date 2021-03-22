7652f060012568e4c8ce7c41a43c416d3bd5533e
LPNHR <lpnhr.style@gmail.com>
2021-03-22 17:43:07 +0500

    Григоричев 7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,12 +1,11 @@
 package com.example.task04;
 
-import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Task04Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         double sum = 0;
         while (scanner.hasNext())
