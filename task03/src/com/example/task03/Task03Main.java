719ae31f9799fd2b3b58b2251e56e3a58896c40a
flumpyz <natalianata2001@mail.ru>
2021-03-28 20:34:05 +0500

    Лебедева Наталья. Задание 7.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,31 @@
 package com.example.task03;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if(charset == null || inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+        int input = inputStream.read();
+        List<Integer> bytes = new ArrayList<>();
+        while(input != -1) {
+            bytes.add(input);
+            input = inputStream.read();
+        }
+        byte[] resultBytes = new byte[bytes.size()];
+        for(int i = 0; i < bytes.size(); i++) {
+            resultBytes[i] = (byte)((int)(bytes.get(i)));
+        }
+        return new String(resultBytes, charset);
     }
 }
