18065ee92e421790f24d46349db12b063bb79557
Vlad <vladpiligrimov@gmail.com>
2021-03-28 18:04:37 +0500

    task03 complete

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,26 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
-        System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
+        System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"),
+                                        Charset.forName("KOI8-R")));
+
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String text = new BufferedReader(new InputStreamReader(inputStream, charset))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        return text;
     }
 }
