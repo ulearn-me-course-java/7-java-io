a10676eb8d2aff028361628d090d47a94d58312f
ebuche-murloc <immortalmagicdend@gmail.com>
2021-03-30 21:09:00 +0500

    Быков_7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,12 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +14,28 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        double sum = 0;
 
-        System.out.println("0.0");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String cur;
+
+        while ((cur = reader.readLine()) != null) {
+            for (String num : cur.split(" ")) {
+                sum += tryParseDouble(num);
+            }
+        }
+
+        System.out.printf(Locale.ENGLISH, "%.6f", sum);
+    }
+
+    public static double tryParseDouble(final String number) {
+
+        double result;
+        try {
+            result = Double.parseDouble(number);
+        } catch (NumberFormatException e) {
+            result = 0.0;
+        }
+        return result;
     }
 }
