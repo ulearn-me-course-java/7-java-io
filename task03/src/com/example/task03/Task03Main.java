2846252fd813b24987974ce48bbbbbc50dd9406e
Wither87 <wither_87@mail.ru>
2021-03-30 15:34:47 +0500

    Крутолевич Сергей. Задача 7.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,6 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -9,13 +8,22 @@ public class Task03Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
+
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null)
+            throw new IllegalArgumentException();
+
+        Reader reader = new InputStreamReader(inputStream, charset);
+        StringBuilder result = new StringBuilder();
+        int value = 0;
+        while ((value = reader.read()) >= 0){
+            char symbol = (char)value;
+            result.append(symbol);
+        }
+        return result.toString();
     }
 }
