bd077fdbf5839e5df33992225bcd7ac067c3b0ad
yulkino <yulkino@hotmail.com>
2021-03-31 18:52:25 +0500

    Task7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,10 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +13,30 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        getSequenceSum();
+    }
+
+    public static void getSequenceSum() {
+        Scanner scan = new Scanner(System.in);
+        String current;
+        double sum = 0;
+        while(scan.hasNext()){
+            if(isNumeric(current = scan.next())) {
+                double d = Double.parseDouble(current);
+                sum += d;
+            }
+        }
+        System.out.print(BigDecimal.valueOf(sum).setScale(6, BigDecimal.ROUND_HALF_UP));
+    }
+
+    public static boolean isNumeric(String str)
+    {
+        try {
+            Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
