c87d09a8f99625a14f1446d243c2d04e16472140
Sergey <sergstas0@gmail.com>
2021-03-31 20:34:21 +0500

    task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,20 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        toUnixNotation(System.in);
+    }
+
+    private static void toUnixNotation(InputStream inputStream) throws IOException {
+        StringBuilder line = new StringBuilder();
+        int chr = inputStream.read();
+        while(chr != -1) {
+            line.append((char) chr);
+            chr = inputStream.read();
+        }
+        System.out.print(line.toString().replaceAll("\r\n", "\n"));
     }
 }
