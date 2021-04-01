ecf340adb95225df23414bc18eb28db23cc1780a
Van Darkholme <jija@kaavo>
2021-04-01 12:26:54 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,23 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
 
-        System.out.println("0.0");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                sum += scanner.nextDouble();
+
+            } else {
+                scanner.next();
+            }
+        }
+
+        System.out.println(BigDecimal.valueOf(sum).setScale(6, BigDecimal.ROUND_HALF_UP));
     }
 }
