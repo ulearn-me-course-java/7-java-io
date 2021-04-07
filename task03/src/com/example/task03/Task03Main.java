2afd4f73a4fd3e3fa0a4662f35f6b1a8e2a30d52
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-04-07 22:37:08 +0500

    Update Task03Main.java

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,27 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException, IllegalArgumentException {
+        if (inputStream == null || charset == null) {
+            throw new IllegalArgumentException("Null argument");
+        }
+        InputStreamReader stream = new InputStreamReader(inputStream, charset);
+        StringBuilder sb = new StringBuilder();
+        char[] chars = new char[128];
+
+        int count = 0;
+        do {
+            count = stream.read(chars);
+            sb.append(chars, 0, count);
+        } while (count == chars.length);
+
+        return sb.toString();
     }
 }
