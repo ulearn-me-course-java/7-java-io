ba81bcf15dbcb6c056f4a8bdbb004f372426099c
пк <nikitos.wie@mail.ru>
2021-04-07 20:57:01 +0300

    Яхин, Task - 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException { //
+    public static void main(String[] args) throws IOException {
 
     }
 
