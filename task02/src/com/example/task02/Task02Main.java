383b872c4dfd3db988eb20cbd9e21559a8801246
Fofan <vovka.322@Mail.ru>
2022-04-06 01:25:14 +0600

    Ефимов Иван task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,6 +4,34 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
+
+        int read = System.in.read();
+
+        while (read > -1) {
+
+            if (read == 13) {
+
+                int temp = System.in.read();
+
+                if (temp == 10) {
+                    read = temp;
+                }
+                else if (temp == -1) {
+                    System.out.write(read);
+                    break;
+                }
+                else {
+                    System.out.write(read);
+                    read = temp;
+                }
+            }
+            else {
+                System.out.write(read);
+                read = System.in.read();
+            }
+        }
+        System.out.flush();
+
         // чтобы протестировать свое решение, вам нужно:
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - направить стандартный вывод программы в файл output.test
