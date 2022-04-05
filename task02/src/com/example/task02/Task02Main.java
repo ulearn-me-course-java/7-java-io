80cb39f53ac5c89a5444f5557edc5b7d813d2ea9
Saveliy <savelussava@mail.ru>
2022-04-05 19:25:37 +0500

    Task2. Кудрявцев Савелий

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,30 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int readByte = System.in.read();
+
+        while (readByte > -1) {
+            if (readByte == 13) {
+                int temp = System.in.read();
+
+                if (temp == 10) {
+                    readByte = temp;
+                }
+                else if (temp == -1) {
+                    System.out.write(readByte);
+
+                    break;
+                }
+                else {
+                    System.out.write(readByte);
+                    readByte = temp;
+                }
+            }
+            else {
+                System.out.write(readByte);
+                readByte = System.in.read();
+            }
+        }
+        System.out.flush();
     }
 }
