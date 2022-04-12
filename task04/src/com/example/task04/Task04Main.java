ead21751f53c91eb89b36cfcc222e74f2bbe9597
Amir <70907990+Amirka2@users.noreply.github.com>
2022-04-13 01:15:01 +0500

    Задание 04. Файзуллин Амир

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,9 @@
 package com.example.task04;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +12,24 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        try{
+            System.setIn(new FileInputStream("task04\\src\\com\\example\\task04\\input.test"));
+        } catch (IOException ignored) {
+        }
+
+        double result = 0;
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.UK);
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                result += scanner.nextDouble();
+            } else {
+                scanner.next();
+            }
+        }
+
+        //System.out.println(result);
+        System.out.printf(Locale.UK, "%.6f", result);
     }
 }
