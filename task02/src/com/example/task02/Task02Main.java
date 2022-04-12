337e4fb89bcb31932de6c93ee0ddfabaebfd7eed
ArsaGit <artyom.arsalanov@gmail.com>
2022-04-13 03:59:08 +0500

    Арсаланов Артём. Задание 7.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
+import java.io.*;
 import java.io.IOException;
+
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,23 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        File input = new File("task02\\src\\com\\example\\task02\\input.test");
+        File output = new File("task02\\src\\com\\example\\task02\\output.test");
+        File expected = new File("task02\\src\\com\\example\\task02\\expected.test");
+        System.setIn(new FileInputStream(input));
+        System.setOut(new PrintStream(new FileOutputStream(output)));
+
+        int first = System.in.read();
+        while (first != -1) {
+            int second = System.in.read();
+            if (first != 13 || second != 10) {
+                System.out.write(first);
+            }
+            first = second;
+        }
+
+        System.out.flush();
+
+
     }
 }
