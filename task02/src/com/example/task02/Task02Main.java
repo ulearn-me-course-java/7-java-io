8274906e54eb35c859f94db88645a3410db06203
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-04-12 22:05:56 +0500

    Бабин Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,14 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int previous = System.in.read();
+        while (previous != -1) {
+            int current = System.in.read();
+            if (previous != '\r' || current != '\n')
+                System.out.write(previous);
+            previous = current;
+        }
+
+        System.out.flush();
     }
 }
