8b68c0a97a0a68e28e5ddffc053f0f92f5ee312e
Nanoster <danil2002.11@mail.ru>
2022-04-13 00:31:02 +0500

    Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,12 +2,21 @@ package com.example.task02;
 
 import java.io.IOException;
 
-public class Task02Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+public class Task02Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        int prevNum = System.in.read();
+
+        while (prevNum != -1)
+        {
+            int currentNum = System.in.read();
+            if (prevNum != '\r' || currentNum != '\n')
+            {
+                System.out.write(prevNum);
+            }
+            prevNum = currentNum;
+        }
+        System.out.flush();
     }
 }
