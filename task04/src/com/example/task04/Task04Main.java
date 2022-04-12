bcc68e7d9a7684796b8c776dcae2244abe91d0fe
ArsaGit <artyom.arsalanov@gmail.com>
2022-04-13 04:25:32 +0500

    Арсаланов Артём. Задание 7.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,9 @@
 package com.example.task04;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +12,18 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        try{
+            System.setIn(new FileInputStream("task04\\src\\com\\example\\task04\\input.test"));
+        }
+        catch (IOException ignored) {}
+
+        double sum = 0;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble())
+                sum += scanner.nextDouble();
+            else scanner.next();
+        }
+        System.out.printf(Locale.ENGLISH, "%.6f", sum);
     }
 }
