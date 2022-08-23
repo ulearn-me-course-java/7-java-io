bebc7b8464b328e6a830d7a42fa821356fa9361c
modina-o@mail.ru <wbXUk*StPRN5Agx>
2022-08-23 11:36:46 +0500

    task02 Модина

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,30 @@
 package com.example.task02;
 
-import java.io.IOException;
+import javafx.concurrent.Task;
+
+import java.io.*;
+import java.nio.file.Files;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
+        InputStream inputStream = in;
+        int previousRead = inputStream.read();
+        int read = 0;
+        while (previousRead >= 0) {
+            read = inputStream.read();
+            if (!(read == 10 && previousRead == 13))
+                out.write(previousRead);
+            previousRead = read;
+        }
+            out.flush();
+    }
         // чтобы протестировать свое решение, вам нужно:
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
-    }
 }
+
