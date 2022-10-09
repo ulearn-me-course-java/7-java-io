3ec686e7a83d3c1f84798a29c0e9d83e1d46fcd5
by_Onyx <you@example.com>
2022-10-09 22:22:33 +0500

    alldone

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,7 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +16,13 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if(inputStream == null) throw new IllegalArgumentException();
+        int sum = 0;
+        int length = inputStream.read();
+        while (length >= 0){
+            sum = Integer.rotateLeft(sum, 1) ^ length;
+            length = inputStream.read();
+        }
+        return sum;
     }
 }
