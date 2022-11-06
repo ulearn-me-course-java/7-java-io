0c742d3788695ad7b4b8e7224fe3268716478c3a
Nigmatulin Timur <timurnigmatulin6@gmail.com>
2022-11-06 22:53:10 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,11 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        double sum = 0d;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        while (scanner.hasNextDouble()) {
+            sum += scanner.nextDouble();
+        }
+        System.out.printf(Locale.ENGLISH, "%.6f", sum);
     }
 }
