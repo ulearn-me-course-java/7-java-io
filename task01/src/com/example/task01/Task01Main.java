08de23ef56488d08d976c43a248ce106800e5059
Настя <nastamezenceva9155@gmail.com>
2022-11-06 18:47:26 +0500

    all

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,27 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
 
     }
 
+
+    //C[n+1]=rotateLeft(C[n]) xor b[n+1]
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if(inputStream == null) throw new IllegalArgumentException();
+        int sum = 0;
+        int length = inputStream.read();
+        while (length != -1){
+            sum = Integer.rotateLeft(sum, 1) ^ length;
+            length = inputStream.read();
+        }
+        return sum;
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,18 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        InputStream is = System.in;
+        if (is == null) throw new IllegalArgumentException();
+        int prev = is.read();
+        int current = is.read();
+        while(prev != -1){
+            if ((prev != 13 || current != 10)){
+                System.out.write(prev);
+            }
+            prev = current;
+            current = is.read();
+        }
+        System.out.flush();
     }
 }
