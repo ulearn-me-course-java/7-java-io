6b9d9ec2049db76161a31e6b100714bd1eac771f
Amir <70907990+Amirka2@users.noreply.github.com>
2022-11-19 03:36:19 +0500

    Модуль 7. Файзуллин Амир. При-201

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,34 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+
+            InputStream inputStream = System.in;
+            OutputStream outputStream = System.out;
+
+//        try (InputStream inputStream = new FileInputStream("task02/src/com/example/task02/input.test");
+//             OutputStream outputStream = new FileOutputStream("task02/src/com/example/task02/output.test")) {
+
+            int prev = inputStream.read();
+
+            while (prev != -1) {
+                int current = inputStream.read();
+                if (prev != '\r' || current != '\n') outputStream.write(prev);
+
+                prev = current;
+            }
+            outputStream.flush();
+//        }
+
+//        BufferedReader reader = Files.newBufferedReader(Paths.get("task02/src/com/example/task02/output.test"));
+//        int x;
+//        while ((x = reader.read()) != -1) {
+//            System.out.print(x + " ");
+//        }
     }
 }
