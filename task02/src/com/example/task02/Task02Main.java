170e37573f3379bdb10ab0703f38921ea0a9fe3c
Архипов Евгений <arkhipov.zena@gmail.com>
2022-11-20 23:15:35 +0500

    I must go

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -8,6 +9,25 @@ public class Task02Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        ConvertEnd(inputStream, outputStream);
+    }
+
+    public static void ConvertEnd(InputStream inputStream, OutputStream outputStream) throws IOException {
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int nextChar;
+        int curChar = inputStream.read();
+        while (curChar != -1) {
+            nextChar = inputStream.read();
+            if (!(curChar == 13 && nextChar == 10))
+                outputStream.write(curChar);
+            curChar = nextChar;
+        }
+        outputStream.flush();
     }
 }
