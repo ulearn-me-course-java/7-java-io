355d61426136e094801cb958bbffeabafc6c5700
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-11-20 21:03:01 +0500

    Task_3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,25 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
+
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null) throw new IllegalArgumentException("inputStream cannot be null");
+        if (charset == null) throw new IllegalArgumentException("charset cannot be null");
+
+        BufferedInputStream bis = new BufferedInputStream(inputStream); //накапливает вводимые данные в специальном буфере без постоянного обращения к устройству ввода.
+        ByteArrayOutputStream buf = new ByteArrayOutputStream(); // входной поток, использующий в качестве источника данных массив байтов.
+
+        for (int result = bis.read(); result != -1; result = bis.read()) {
+            buf.write((byte) result);
+        }
+
+        return buf.toString(String.valueOf(charset));
     }
 }
