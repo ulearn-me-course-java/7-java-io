ff3a5dd6c9d22f0b7e044a010daf58f14eac61f0
Мелёхина Виктория <sweet_fox7940107@mail.ru>
2022-11-20 22:41:41 +0500

    all

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,19 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        InputStream reading = System.in;
+        if (reading == null) {
+            throw new NullPointerException();
+        }
+        int prev = reading.read();
+        int current = reading.read();
+        while(prev > -1){
+            if ((prev != 13 || current != 10)){
+                System.out.write(prev);
+            }
+            prev = current;
+            current = reading.read();
+        }
+        System.out.flush();
     }
 }
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,7 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +9,22 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        InputStream reading = System.in;
+        String readValue = Integer.toString(reading.read());
+        double sumDub = 0;
+        double numDub = 0.0;
 
-        System.out.println("0.0");
+        while (readValue != "-1") {
+
+            try {
+                numDub = Double.parseDouble(readValue);
+            } catch (NumberFormatException e) {
+                numDub = 0.0;
+            }
+            sumDub += numDub;
+            readValue = Integer.toString(reading.read());
+
+        }
+        System.out.println(String.format("%.6f", sumDub));
     }
 }
