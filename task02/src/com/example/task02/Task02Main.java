d6305fb218183ce1f713d89862d92b46f50cdd7f
Vilena <vilena.saitgalina@mail.ru>
2022-11-20 20:32:32 +0500

    Саитгалина Вилена task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,17 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        InputStream inputStream = System.in;
+        int current = inputStream.read();
+        int next = inputStream.read();
+        while (current > -1) {
+            if (!(current == 13 && next == 10)) {
+                System.out.write(current);
+            }
+            current = next;
+            next = inputStream.read();
+        }
+        System.out.flush();
     }
 }
