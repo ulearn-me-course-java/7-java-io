51a8971ac5f331d48449a5cf5e98a6a32c701030
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-11-20 20:36:34 +0500

    Task_2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,20 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        InputStream inputStream = System.in; // это простая статическая переменная типа InputStream
+
+        int current = inputStream.read();
+        int next = inputStream.read();
+
+        while (current != -1) {
+            if (!(current == 13 && next == 10)) System.out.write(current);
+            current = next;
+            next = inputStream.read();
+        }
+        System.out.flush();
     }
 }
