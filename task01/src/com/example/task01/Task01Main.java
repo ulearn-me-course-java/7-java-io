117ea9062755c92a40fb3d89733c55936b7ec9c4
Alina <wbrflf3301@outlook.com>
2022-11-20 19:31:24 +0500

    Выполнено задание 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,5 +1,6 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,14 +9,18 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if(inputStream == null) throw new IllegalArgumentException();
+
+        int _byte = inputStream.read();
+        int sum = 0;
+        while(_byte != -1) {
+            sum = Integer.rotateLeft(sum, 1) ^ (byte)_byte;
+            _byte = inputStream.read();
+        }
+        return sum;
     }
 }
