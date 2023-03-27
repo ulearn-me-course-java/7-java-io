917416632533f740dee40c1570d4c807b1a5c458
Maxim <maksimevs2003@gmail.com>
2023-03-27 09:35:41 +0500

    Евстигнеев Максим. Задание 07

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,7 @@ package com.example.task02;
 
 import java.io.IOException;
 
+
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         // чтобы протестировать свое решение, вам нужно:
@@ -9,5 +10,16 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int read = System.in.read();
+        int next = System.in.read();
+        while (read != -1) {
+            if (read != 13 || next != 10) {
+                System.out.write(read);
+            }
+            read = next;
+            next = System.in.read();
+        }
+
+        System.out.flush();
     }
 }
