5880018550d4480dc566974e6d584e65c9d48b36
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-09-25 20:03:15 +0500

    done

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,21 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.UK);
+
+        double sum = 0d;
+        while (sc.hasNextDouble()) {
+            sum += sc.nextDouble();
+        }
+
+        System.out.print(String.format(Locale.UK,"%.6f", sum));
+
     }
 }
