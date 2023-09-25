bc7955bcaefc359c8237cbd991a55cf374a5ebf6
Michael Gallinago <michael.gallinago@gmail.com>
2023-09-26 01:15:58 +0500

    Update testing

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,7 +3,18 @@ package com.example.task02;
 import java.io.IOException;
 
 public class Task02Main {
+
+    private static boolean isTest = false;
+
     public static void main(String[] args) throws IOException {
+        if (isTest) {
+            convertStringWindowsToUnix();
+        } else {
+            test();
+        }
+    }
+
+    private static void convertStringWindowsToUnix() throws IOException {
         int currentSymbol = System.in.read();
 
         while (currentSymbol != -1) {
@@ -16,11 +27,13 @@ public class Task02Main {
         }
 
         System.out.flush();
+    }
 
-        /*
+    private static void test() throws IOException {
+        isTest = true;
         Tests tests = new Tests();
         tests.testExample();
         tests.testEmpty();
-        tests.testArrays();*/
+        tests.testArrays();
     }
 }
