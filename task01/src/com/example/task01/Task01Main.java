d36cf5cf11c5a4204b1b915f0b44f82377eb2fef
informant <artemiy.solodkin@mail.ru>
2023-09-27 00:02:28 +0500

    2 DOne

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,5 +1,6 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,14 +9,29 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
-        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
 
+        //System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
+
+        Tests tests = new Tests();
+        tests.testArrays();
+        tests.testExample();
+        //tests.testException();
+        tests.testNull();
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
         // your implementation here
-        return 0;
+        if (inputStream == null){
+            throw new IllegalArgumentException();
+        }
+        int res = 0;
+        int readLength = 0;
+        byte[] b = new byte[1024];
+        while ((readLength = inputStream.read(b))>0){
+            for (int i=0; i<readLength;i++){
+                res = Integer.rotateLeft(res,1) ^ b[i];
+            }
+        }
+        return res;
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,6 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +9,23 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+
+        windowsEndLineToUnixEndLine(inputStream,outputStream);
+    }
+    public static void windowsEndLineToUnixEndLine(InputStream inputStream,OutputStream outputStream) throws IOException {
+        int readLength = 0;
+        byte[] read = new byte[1024];
+        while ((readLength = inputStream.read(read))>0){
+            for (int i=0; i<readLength-1;i++){
+                if (!(read[i] == 13 && read[i+1] == 10)){
+                    outputStream.write(read[i]);
+                }
+            }
+            outputStream.write(read[readLength-1]);
+        }
+        System.out.flush();
     }
 }
