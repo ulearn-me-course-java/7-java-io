75f6ce6445e645bb0a230a0d55ff1c8cd8195836
Sug4chy <sb.9226981433@gmail.com>
2023-10-06 21:36:02 +0500

    Бойко Степан. Все задания

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,35 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
+        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0, 9, 8, 7, 6, 5, 4, 3, 2, 1})));
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+        List<Byte> bytes = new ArrayList<>();
+        do {
+            bytes.add((byte) inputStream.read());
+        } while (bytes.get(bytes.size() - 1) >= 0);
+        bytes.remove(bytes.size() - 1);
+        return getSumOfStream(bytes.toArray(new Byte[0]), bytes.size() - 1);
+    }
+
+    private static int getSumOfStream(Byte[] bytes, int index) {
+        if (bytes.length == 0) {
+            return 0;
+        }
+        if (index == 0) {
+            return bytes[0];
+        }
+        return Integer.rotateLeft(getSumOfStream(bytes, index - 1), 1) ^ bytes[index];
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,28 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        changeNewLineCharFromWindowsToUnix();
+    }
+
+    private static void changeNewLineCharFromWindowsToUnix() throws IOException {
+        List<Byte> inputBytes = new ArrayList<>();
+        do {
+            inputBytes.add((byte) System.in.read());
+        } while (inputBytes.get(inputBytes.size() - 1) >= 0);
+        inputBytes.remove(inputBytes.size() - 1);
+        Byte[] input = inputBytes.toArray(new Byte[0]);
+        for (int i = 0; i < input.length; i++) {
+            if (!(i == input.length - 1) && input[i] == 13 && input[i + 1] == 10) {
+                System.out.write(10);
+                i++;
+                continue;
+            }
+            System.out.write(inputBytes.get(i));
+        }
     }
 }
