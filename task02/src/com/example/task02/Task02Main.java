2fd2375ca26deb275808388ab9ea3f6938e46c31
Diana <lici4qweru090@gmail.com>
2023-11-06 03:37:48 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,24 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        //InputStream inputStream = new FileInputStream("task02/src/com/example/task02/input.test");
+        //OutputStream outputStream = new FileOutputStream("task02/src/com/example/task02/output.test");
+
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        int previous = inputStream.read();
+        int current = inputStream.read();
+        while (previous > -1) {
+            if(!(previous == 13 && current == 10)) {
+                outputStream.write(previous);
+            }
+            previous = current;
+            current = inputStream.read();
+        }
+        outputStream.flush();
     }
 }
