8d7c7631cb546c611fe6332968f874eb224fc33e
KatyaPark11 <127592285+KatyaPark11@users.noreply.github.com>
2023-11-05 23:12:06 +0500

    Федотова Екатерина. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,21 @@
 package com.example.task03;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if(inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset));
+        return reader.readLine();
     }
 }
