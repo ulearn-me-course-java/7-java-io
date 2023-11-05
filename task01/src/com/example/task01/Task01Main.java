6cdb003d82773ed636c8ef50d46eb828a6ab4a7c
KatyaPark11 <127592285+KatyaPark11@users.noreply.github.com>
2023-11-05 23:11:44 +0500

    Федотова Екатерина. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,26 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException("inputStream was null");
+        }
+
+        byte[] arr = new byte[1024];
+        int len = inputStream.read(arr);
+        return getSum(arr, len - 1);
+    }
+
+    public static int getSum(byte[] arr, int byteIndex) {
+        if (byteIndex <= 0) return arr[0];
+        return Integer.rotateLeft(getSum(arr, byteIndex - 1), 1) ^ arr[byteIndex];
     }
 }
