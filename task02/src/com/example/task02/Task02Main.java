25a8beee02dbbdd06dea7d457ac2cfd49e473865
KatyaPark11 <127592285+KatyaPark11@users.noreply.github.com>
2023-11-05 23:11:54 +0500

    Федотова Екатерина. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,17 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int read = System.in.read();
+        int next = System.in.read();
+
+        while (read != -1){
+            if (read != 13 || next != 10) {
+                System.out.write(read);
+            }
+            read = next;
+            next = System.in.read();
+        }
+
+        System.out.flush();
     }
 }
