ab26a856aa81f301cb620da38de1b566cd4f3d29
Elizaveta Pernikova <lisapernikowa@mail.ru>
2023-11-05 21:11:53 +0500

    Task01-Task04

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,37 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        int current = System.in.read();
+        while(current != -1){
+            int next = System.in.read();
+            if(current == 13 && next == 10){
+                System.out.write(10);
+                current = System.in.read();
+                continue;
+            }
+            System.out.write(current);
+            current = next;
+        }
+        System.out.flush();
+        //пример для работы с файлом:
+        //readLines(new FileReader("task02/src/com/example/task02/input.test"),
+        //        new FileWriter("task02/src/com/example/task02/output.test"));
     }
+
+    public static void readLines(Reader reader, Writer writer) throws IOException{
+        int current = reader.read();
+        while(current != -1){
+            int next = reader.read();
+            if(current == 13 && next == 10){
+                writer.write(10);
+                current = reader.read();
+                continue;
+            }
+            writer.write(current);
+            current = next;
+        }
+        writer.flush();
+    }
+
 }
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,12 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.Scanner;
+
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +14,33 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        if(System.in == null){
+            throw new IllegalArgumentException();
+        }
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        double num = 0.0d;
+        while(scanner.hasNext()) {
+            try{
+                num += Double.parseDouble(scanner.next());
+            }catch (NumberFormatException ignored){}
+        }
+        System.out.printf(Locale.ENGLISH, "%.6f%n", num);
+        //пример для работы с файлом:
+        //System.out.printf(Locale.ENGLISH, "%.6f%n",
+        //      readLines(Files.newInputStream(Paths.get("task04/src/com/example/task04/input.test"))));
+    }
 
-        System.out.println("0.0");
+    public static double readLines(InputStream inputStream) throws IOException{
+        if(inputStream == null){
+            throw new IllegalArgumentException();
+        }
+        Scanner scanner = new Scanner(inputStream).useLocale(Locale.ENGLISH);
+        double num = 0.0d;
+        while(scanner.hasNext()) {
+            try{
+                num += Double.parseDouble(scanner.next());
+            }catch (NumberFormatException ignored){}
+        }
+        return num;
     }
 }
