4c6bca83c217bee9a63ac9ad0f8fe8df1c6792b3
Polina Starodubtseva <akulam75@gmail.com>
2023-11-06 06:42:06 +0500

    IOthreads well done

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,19 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+
+        int previous = inputStream.read();
+        int actual = inputStream.read();
+
+        while (previous > -1) {
+            if(!(previous == 13 && actual == 10)) {
+                outputStream.write(previous);
+            }
+            previous = actual;
+            actual = inputStream.read();
+        }
+        outputStream.flush();
     }
 }
