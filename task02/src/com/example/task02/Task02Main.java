30b72441628d0de283b8b635c2c92929fe9a0f73
Dell <Dell@DESKTOP-JRO69R2>
2023-11-06 16:49:29 +0300

    new

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,24 @@
 package com.example.task02;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+    public static void main(String[] args) throws IOException
+    {
+
+        int previous = System.in.read();
+        while (previous != -1) {
+            int current = System.in.read();
+            if (previous != '\r' || current != '\n')
+                System.out.write(previous);
+            previous = current;
+        }
+
+        System.out.flush();
     }
 }
