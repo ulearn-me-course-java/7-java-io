d97d4eae746bf128b8e8298de626b2aa2ce62048
LeeLuiza <116955531+LeeLuiza@users.noreply.github.com>
2023-11-06 09:48:03 +0200

    lab

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,9 +1,28 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.*;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
+        //InputStream inputStream = new FileInputStream("task02/src/com/example/task02/input.test");
+        //OutputStream outputStream = new FileOutputStream("task02/src/com/example/task02/output.test");
+
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+
+        int previous = inputStream.read();
+        int current = inputStream.read();
+
+        while (previous > -1) {
+            if(!(previous == 13 && current == 10)) { //\r \n
+                outputStream.write(previous);
+            }
+            previous = current;
+            current = inputStream.read();
+        }
+        System.out.flush();
+
         // чтобы протестировать свое решение, вам нужно:
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - направить стандартный вывод программы в файл output.test
