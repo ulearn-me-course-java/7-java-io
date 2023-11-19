26167e97d0bbc12f9d8be6dc13cd4b2e5e03d926
vadim471 <tissen.vadimka@mail.ru>
2023-11-20 04:58:00 +0500

    done

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,10 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +12,19 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        InputStream inputStream = System.in;
+        System.out.println(String.format("%.6f", sumOfDouble(inputStream)));
+    }
+    public static double sumOfDouble(InputStream inputStream){
+        double sum = 0;
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNextLine()) {
+            try {
+                sum += Double.parseDouble(scanner.next());
+            } catch (Exception e) {
 
-        System.out.println("0.0");
+            }
+        }
+        return sum;
     }
 }
