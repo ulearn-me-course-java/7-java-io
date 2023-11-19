d1183ea1dbceb1c8ad883c4a1b20be3b07a74148
Pain-Excalibur <ffggh.fn2@gmail.com>
2023-11-19 19:30:50 +0500

    1

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,34 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        //write to list
+        int nextByte;
+        ArrayList<Integer> nextBytes = new ArrayList<>();
+        while ((nextByte = System.in.read()) != -1)
+        {
+            nextBytes.add(nextByte);
+        }
+
+        //replace 13 10 with 10
+        int i = 0;
+        while(i < nextBytes.size()-1)
+        {
+            if (nextBytes.get(i) == 13 && nextBytes.get(i+1) == 10)
+            {
+                nextBytes.remove(i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        //print list
+        for (int elem : nextBytes)
+        {
+            System.out.write(elem);
+        }
+        System.out.flush();
     }
 }
