7e1a3a06a367c5275b9c00b5ee86d37466ee7257
Tiffecko <113545856+Tiffecko@users.noreply.github.com>
2023-11-19 16:27:08 +0500

    Яшин А. ПрИ-201

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,22 @@
 package com.example.task04;
 
-import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+    public static void main(String[] args)  {
+        Sum(System.in);
+    }
 
-        System.out.println("0.0");
+    public static void Sum(InputStream inputStream){
+        Scanner scanner = new Scanner(inputStream).useLocale(Locale.US);
+
+        double sum = 0.0;
+        while (scanner.hasNextDouble()) {
+            sum += scanner.nextDouble();
+        }
+
+        System.out.printf(Locale.US, "%.6f", sum);
     }
 }
