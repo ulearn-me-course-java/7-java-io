b8494b6d15532d706ebe00e59d03f512beed965d
D00M33D <al3ksey.19@gmail.com>
2024-02-10 23:15:01 +0500

    Completed

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,11 +3,20 @@ package com.example.task02;
 import java.io.IOException;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+    public static void main(String[] args) throws IOException { changeFormat(); }
+
+
+        public static void changeFormat() throws IOException {
+            int current = System.in.read();
+            int next;
+
+            while(current != -1){
+                next = System.in.read();
+                if (!(current == 13 && next == 10)){
+                    System.out.write(current);
+                }
+                current = next;
+            }
+            System.out.flush();
+        }
     }
-}
